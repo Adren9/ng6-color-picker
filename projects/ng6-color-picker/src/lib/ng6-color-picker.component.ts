99633d958dc27baab0d7f1987d5d2222cacf5ffc
Adren9 <przemek_rogas@wp.pl>
2018-06-07 00:34:21 +0200

    fix urls of template and style in main component

--- projects/ng6-color-picker/src/lib/ng6-color-picker.component.ts
+++ projects/ng6-color-picker/src/lib/ng6-color-picker.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'adr-ng6-color-picker',
-  templateUrl: './color-picker.component.html',
-  styleUrls: ['./color-picker.component.css']
+  templateUrl: './ng6-color-picker.component.html',
+  styleUrls: ['./ng6-color-picker.component.css']
 })
 export class Ng6ColorPickerComponent implements OnInit {
 
