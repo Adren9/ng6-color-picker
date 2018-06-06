82b772f08c21b08a84ebfdb72118ede0ba9f450d
Adren9 <przemek_rogas@wp.pl>
2018-06-07 00:47:55 +0200

    Add directive input (container against which pointer position will be calculated)

--- projects/ng6-color-picker/src/lib/directives/pointer.directive.ts
+++ projects/ng6-color-picker/src/lib/directives/pointer.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[adrPointer]'
 })
-export class PointerDirective {
+export class PointerDirective implements OnInit {
+
+  // Element against which pointer position is calculated
+  @Input('container') container;
 
   constructor() { }
+
+  ngOnInit() {
+    if (! this.container) {
+      throw new Error('Container element is not specified.');
+    }
+  }
 
 }
