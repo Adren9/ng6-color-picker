6d94af328266ac408ea5efe40edddf979e7a706e
Adren9 <przemek_rogas@wp.pl>
2018-06-07 00:21:15 +0200

    remove unnecessary service

--- projects/ng6-color-picker/src/public_api.ts
+++ projects/ng6-color-picker/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ng6-color-picker
  */
 
-export * from './lib/ng6-color-picker.service';
 export * from './lib/ng6-color-picker.component';
 export * from './lib/ng6-color-picker.module';
