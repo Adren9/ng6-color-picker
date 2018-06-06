b211d47b422b3db5cd82742ca0832541f51d3ecd
Adren9 <przemek_rogas@wp.pl>
2018-06-07 00:40:02 +0200

    create initial pointer directive, it will be used to pick values from picker and sliders

---
+++ projects/ng6-color-picker/src/lib/directives/pointer.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[adrPointer]'
+})
+export class PointerDirective {
+
+  constructor() { }
+
+}
--- projects/ng6-color-picker/src/lib/ng6-color-picker.module.ts
+++ projects/ng6-color-picker/src/lib/ng6-color-picker.module.ts
@@ -3,11 +3,12 @@ import { Ng6ColorPickerComponent } from './ng6-color-picker.component';
 import { SvPickerComponent } from './components/sv-picker/sv-picker.component';
 import { HueSliderComponent } from './components/hue-slider/hue-slider.component';
 import { OpacitySliderComponent } from './components/opacity-slider/opacity-slider.component';
+import { PointerDirective } from './directives/pointer.directive';
 
 @NgModule({
   imports: [
   ],
-  declarations: [Ng6ColorPickerComponent, SvPickerComponent, HueSliderComponent, OpacitySliderComponent],
+  declarations: [Ng6ColorPickerComponent, SvPickerComponent, HueSliderComponent, OpacitySliderComponent, PointerDirective],
   exports: [Ng6ColorPickerComponent]
 })
 export class Ng6ColorPickerModule { }
