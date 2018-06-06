54d03f50e05cc403f5ccb217b06ce22f9b7f0b29
Adren9 <przemek_rogas@wp.pl>
2018-06-07 00:27:31 +0200

    generate initial hue slider component

---
+++ projects/ng6-color-picker/src/lib/components/hue-slider/hue-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'adr-hue-slider',
+  templateUrl: './hue-slider.component.html',
+  styleUrls: ['./hue-slider.component.css']
+})
+export class HueSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
