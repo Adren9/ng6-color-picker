39de8a2d0cc7d3ee3f803f5bc0dbf5c139f170a9
Adren9 <przemek_rogas@wp.pl>
2018-06-07 00:28:36 +0200

    generate inital opacity slider

---
+++ projects/ng6-color-picker/src/lib/components/opacity-slider/opacity-slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'adr-opacity-slider',
+  templateUrl: './opacity-slider.component.html',
+  styleUrls: ['./opacity-slider.component.css']
+})
+export class OpacitySliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- projects/ng6-color-picker/src/lib/ng6-color-picker.module.ts
+++ projects/ng6-color-picker/src/lib/ng6-color-picker.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Ng6ColorPickerComponent } from './ng6-color-picker.component';
 import { SvPickerComponent } from './components/sv-picker/sv-picker.component';
 import { HueSliderComponent } from './components/hue-slider/hue-slider.component';
+import { OpacitySliderComponent } from './components/opacity-slider/opacity-slider.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [Ng6ColorPickerComponent, SvPickerComponent, HueSliderComponent],
+  declarations: [Ng6ColorPickerComponent, SvPickerComponent, HueSliderComponent, OpacitySliderComponent],
   exports: [Ng6ColorPickerComponent]
 })
 export class Ng6ColorPickerModule { }
