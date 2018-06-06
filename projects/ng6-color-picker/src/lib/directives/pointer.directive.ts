7f9d67fc2a85689d018d7fc6a9a734c39243d81a
Adren9 <przemek_rogas@wp.pl>
2018-06-07 00:50:13 +0200

    inject ElementRef property and added initial styles for pointer and container

--- projects/ng6-color-picker/src/lib/directives/pointer.directive.ts
+++ projects/ng6-color-picker/src/lib/directives/pointer.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, OnInit } from '@angular/core';
+import { Directive, Input, OnInit, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[adrPointer]'
@@ -8,12 +8,15 @@ export class PointerDirective implements OnInit {
   // Element against which pointer position is calculated
   @Input('container') container;
 
-  constructor() { }
+  constructor(private pointer: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
     if (! this.container) {
       throw new Error('Container element is not specified.');
     }
+
+    this.renderer.setStyle(this.container, 'position', 'relative');
+    this.renderer.setStyle(this.pointer.nativeElement, 'position', 'absolute');
   }
 
 }
