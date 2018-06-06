d82de127022a5ed706799b87aa096a85f1fa2a03
Adren9 <przemek_rogas@wp.pl>
2018-06-07 01:33:21 +0200

    create method to calculate mouse coordinates on the basis of event properties and container properties

--- projects/ng6-color-picker/src/lib/directives/pointer.directive.ts
+++ projects/ng6-color-picker/src/lib/directives/pointer.directive.ts
@@ -30,6 +30,7 @@ export class PointerDirective implements OnInit {
   }
 
   onContainerMouseDown(e: MouseEvent) {
+    this.getMouseCoordinates(e);
     this.dragStart();
   }
 
@@ -55,6 +56,42 @@ export class PointerDirective implements OnInit {
   }
 
   onPointerDrag(e: MouseEvent) {
+    this.getMouseCoordinates(e);
+  }
 
+  // Calculates x and y values from event properties and container properties
+  getMouseCoordinates(e: MouseEvent) {
+
+    // Distances from page start
+    const mouseLeft = e.pageX;
+    const mouseTop = e.pageY;
+    const containerLeft = this.container.offsetLeft;
+    const containerTop = this.container.offsetTop;
+
+    const containerWidth = parseInt(window.getComputedStyle(this.container).width, 10);
+    const containerHeight = parseInt(window.getComputedStyle(this.container).height, 10);
+
+    // Mouse positions against container
+    let x = mouseLeft - containerLeft;
+    let y = mouseTop - containerTop;
+
+    // Invert y to be real value of y-axis
+    y = containerHeight - y;
+
+    // values can't be outside of the container
+    if (x < 0) {
+      x = 0;
+    } else if (x > containerWidth) {
+      x = containerWidth;
+    }
+
+    if (y < 0) {
+      y = 0;
+    } else if (y > containerHeight) {
+      y = containerHeight;
+    }
+
+    this.x = x;
+    this.y = y;
   }
 }
