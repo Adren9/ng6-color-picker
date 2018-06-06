ca140f3799cac28344c55bfd3f8230a3f871a9f8
Adren9 <przemek_rogas@wp.pl>
2018-06-07 00:36:50 +0200

    import module in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Ng6ColorPickerModule } from 'ng6-color-picker';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng6ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
