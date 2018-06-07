f64f95c61fb52abbfc8519b3b5260cb8d832e5ff
Adren9 <przemek_rogas@wp.pl>
2018-06-07 16:47:28 +0200

    taking pointer position and picker background moved to ngOnChanges method

--- projects/ng6-color-picker/src/lib/components/sv-picker/sv-picker.component.ts
+++ projects/ng6-color-picker/src/lib/components/sv-picker/sv-picker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output, ViewChild, OnChanges } from '@angular/core';
 import { ColorConverterService } from '../../services/color-converter.service';
 
 
@@ -7,7 +7,7 @@ import { ColorConverterService } from '../../services/color-converter.service';
   templateUrl: './sv-picker.component.html',
   styleUrls: ['./sv-picker.component.css']
 })
-export class SvPickerComponent implements OnInit {
+export class SvPickerComponent implements OnInit, OnChanges {
 
   @Input() hue: number;
   @Input() saturation: number;
@@ -18,9 +18,25 @@ export class SvPickerComponent implements OnInit {
 
   @ViewChild('picker') picker;
 
+  pointerX: number;
+  pointerY: number;
+  pickerStyle;
+
   constructor(private converter: ColorConverterService) { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes) {
+    if (changes['hue']) {
+      this.getPickerStyle();
+    }
+    if (changes['saturation']) {
+      this.getPointerX();
+    }
+    if (changes['value']) {
+      this.getPointerY();
+    }
   }
 
   onPointerPositionChange(position) {
@@ -44,7 +60,7 @@ export class SvPickerComponent implements OnInit {
       v: 100,
     }, true);
 
-    return {
+    this.pickerStyle = {
       backgroundColor: '#' + bgColor
     };
   }
@@ -52,12 +68,12 @@ export class SvPickerComponent implements OnInit {
   getPointerX() {
     const pickerWidth = parseInt(window.getComputedStyle(this.picker.nativeElement).width, 10);
     // Get x position in pixels for current saturation
-    return this.saturation / 100 * pickerWidth;
+    this.pointerX = this.saturation / 100 * pickerWidth;
   }
 
   getPointerY() {
     const pickerHeight = parseInt(window.getComputedStyle(this.picker.nativeElement).height, 10);
     // Get y position in pixels for current value
-    return this.value / 100 * pickerHeight;
+    this.pointerY = this.value / 100 * pickerHeight;
   }
 }
