54083785c506f99d7b5f874a63e36ceb2694eef1
Adren9 <przemek_rogas@wp.pl>
2018-06-07 19:41:55 +0200

    taking pointer position on opacity changes

--- projects/ng6-color-picker/src/lib/components/opacity-slider/opacity-slider.component.ts
+++ projects/ng6-color-picker/src/lib/components/opacity-slider/opacity-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, OnChanges, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, OnChanges, EventEmitter, ViewChild } from '@angular/core';
 import { ColorConverterService } from '../../services/color-converter.service';
 
 @Component({
@@ -9,34 +9,49 @@ import { ColorConverterService } from '../../services/color-converter.service';
 export class OpacitySliderComponent implements OnInit, OnChanges {
 
   @Input() hue: number;
-  @Input() opacity: number = 0.4;
+  @Input() opacity: number;
 
   @Output('change') change = new EventEmitter<number>();
 
+  @ViewChild('slider') slider;
+
   sliderBackgroundGradient: string;
+  pointerX: number;
 
   constructor(private converter: ColorConverterService) {
     this.getSliderBackgroundGradient();
   }
 
   ngOnInit() {
+    this.getPointerX();
   }
 
   ngOnChanges(changes) {
     if (changes['hue']) {
       this.onHueChange();
     }
+    if (changes['opacity']) {
+      this.onOpacityChange();
+    }
   }
 
   onPointerPositionChange(position) {
     const x = position.x.percentage;
     this.opacity = x / 100;
-
     this.change.emit(this.opacity);
   }
 
   onHueChange() {
     this.getSliderBackgroundGradient();
+  }
+
+  onOpacityChange() {
+    this.getPointerX();
+  }
+
+  getPointerX() {
+    const sliderWidth = parseInt(window.getComputedStyle(this.slider.nativeElement).width, 10);
+    this.pointerX = this.opacity * sliderWidth;
   }
 
   getSliderBackgroundGradient() {
@@ -47,7 +62,6 @@ export class OpacitySliderComponent implements OnInit, OnChanges {
     }, true);
 
     this.sliderBackgroundGradient = `linear-gradient(to right, transparent, ${gradientColor})`;
-    console.log(this.sliderBackgroundGradient);
   }
 
 }
