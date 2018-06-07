5b882650ce84eb832d1969723779d12e7a4b48be
Adren9 <przemek_rogas@wp.pl>
2018-06-07 19:21:13 +0200

    add background gradient of slider on the basis of current hue

--- projects/ng6-color-picker/src/lib/components/opacity-slider/opacity-slider.component.ts
+++ projects/ng6-color-picker/src/lib/components/opacity-slider/opacity-slider.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { ColorConverterService } from '../../services/color-converter.service';
 
 @Component({
   selector: 'adr-opacity-slider',
   templateUrl: './opacity-slider.component.html',
   styleUrls: ['./opacity-slider.component.css']
 })
-export class OpacitySliderComponent implements OnInit {
+export class OpacitySliderComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  @Input() hue: number;
+
+  sliderBackgroundGradient: string;
+
+  constructor(private converter: ColorConverterService) {
+    this.getSliderBackgroundGradient();
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes) {
+    if (changes['hue']) {
+      this.onHueChange();
+    }
+  }
+
+  onHueChange() {
+    this.getSliderBackgroundGradient();
+  }
+
+  getSliderBackgroundGradient() {
+    const gradientColor = '#' + this.converter.hsvToRgb({
+      h: this.hue,
+      s: 100,
+      v: 100
+    }, true);
+
+    this.sliderBackgroundGradient = `linear-gradient(to right, transparent, ${gradientColor})`;
+    console.log(this.sliderBackgroundGradient);
   }
 
 }
