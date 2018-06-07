93bb5f0ef22e8da2cb89477d6dd9f2fe369e13f9
Adren9 <przemek_rogas@wp.pl>
2018-06-07 17:34:32 +0200

    changing pointer background color on the basis of current hue

--- projects/ng6-color-picker/src/lib/components/hue-slider/hue-slider.component.ts
+++ projects/ng6-color-picker/src/lib/components/hue-slider/hue-slider.component.ts
@@ -1,24 +1,52 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { ColorConverterService } from '../../services/color-converter.service';
 
 @Component({
   selector: 'adr-hue-slider',
   templateUrl: './hue-slider.component.html',
   styleUrls: ['./hue-slider.component.css']
 })
-export class HueSliderComponent implements OnInit {
+export class HueSliderComponent implements OnInit, OnChanges {
 
-  @Input() hue: number;
+  @Input() hue = 0;
 
   @Output('change') change = new EventEmitter<number>();
 
-  constructor() { }
+  pointerStyle;
+
+  constructor(private converter: ColorConverterService) {
+    this.getPointerStyle();
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes) {
+    if (changes['hue']) {
+      this.onHueChange();
+    }
+  }
+
+  onHueChange() {
+    this.getPointerStyle();
   }
 
   onPointerPositionChange(position) {
     const x = position.x.percentage;
     this.hue = Math.round(x / 100 * 360);
+    this.onHueChange();
     this.change.emit(this.hue);
+  }
+
+  getPointerStyle() {
+    const bgColor = this.converter.hsvToRgb({
+      h: this.hue,
+      s: 100,
+      v: 100
+    }, true);
+
+    this.pointerStyle = {
+      backgroundColor: '#' + bgColor
+    };
   }
 }
