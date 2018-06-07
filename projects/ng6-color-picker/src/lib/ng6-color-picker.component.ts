8911486d029ecca9f9b7eb392f432efdbe57c1a3
Adren9 <przemek_rogas@wp.pl>
2018-06-07 17:02:30 +0200

    bind main component to sv-picker

--- projects/ng6-color-picker/src/lib/ng6-color-picker.component.ts
+++ projects/ng6-color-picker/src/lib/ng6-color-picker.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RGB } from 'ng6-color-picker/lib/models/rgb';
+import { HSV } from './models/hsv';
+import { ColorConverterService } from './services/color-converter.service';
 
 @Component({
   selector: 'adr-ng6-color-picker',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Ng6ColorPickerComponent implements OnInit {
 
-  constructor() { }
+  rgb: RGB;
+  hsv: HSV = { h: 360, s: 100, v: 100 };
+
+  constructor(private converter: ColorConverterService) {
+    this.updateRgb();
+  }
 
   ngOnInit() {
+  }
+
+  private updateRgb() {
+    this.rgb = <RGB>this.converter.hsvToRgb(this.hsv);
+  }
+
+  onHsvChange() {
+    this.updateRgb();
   }
 
 }
