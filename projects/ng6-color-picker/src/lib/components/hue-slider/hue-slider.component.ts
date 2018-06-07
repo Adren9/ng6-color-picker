c15f6e7a5ce0c96cf62ed84c83443e6c0e8476f2
Adren9 <przemek_rogas@wp.pl>
2018-06-07 17:23:19 +0200

    add pointer to hue-slider

--- projects/ng6-color-picker/src/lib/components/hue-slider/hue-slider.component.ts
+++ projects/ng6-color-picker/src/lib/components/hue-slider/hue-slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'adr-hue-slider',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HueSliderComponent implements OnInit {
 
+  @Input() hue: number;
+
+  @Output('change') change = new EventEmitter<number>();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onPointerPositionChange(position) {
+    const x = position.x.percentage;
+    this.hue = Math.round(x / 100 * 360);
+    this.change.emit(this.hue);
+  }
 }
